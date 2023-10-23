765620ae3fe7c607dcc82987252785f3920bd7f3
Zohair Badshah <zohairbadshah11@gmail.com>
2023-10-23 13:10:47 +0530

    Update sideBar.js resume

--- portfolio/src/components/sideBar.js
+++ portfolio/src/components/sideBar.js
@@ -76,7 +76,7 @@ const SideBar = () => {
             transition: "background .3s",
             borderRadius: "5px",
           }}
-          href="https://drive.google.com/file/d/1zBx6Xc9MEcMfhFZU0LjbvjgKJMSUTLpe/view?usp=sharing"
+          href="https://drive.google.com/file/d/17HETOLQOLw2HctqPZQ6N5gABR9EGGTLC/view?usp=sharing"
           target="_blank"
           rel="noreferrer"
         >
